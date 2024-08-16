8b7b4ee273b1f43db8e8afa86dfcf1c385ff2a7d
JoaoDias <joaodiasworking@gmail.com>
2024-08-15 22:56:19 -0300

    feat: ASPNET Core and Angular from scratch course S3M19.

--- Projects/DatingApp/API/Program.cs
+++ Projects/DatingApp/API/Program.cs
@@ -1,6 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+  opt.UseSqlite(builder.Configuration
+  .GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
